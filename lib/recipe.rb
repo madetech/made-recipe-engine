f393e1e2cd7b02dc2cee3893f3aa73d38b1d93c8
Seb Ashton <seb@madebymade.co.uk>
2013-03-21 17:52:21 +0000

    use mattr_accessor

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,15 @@
 require "recipe/engine"
 
 module Recipe
+  mattr_accessor :recipe_item_sweeper
+  @@recipe_item_sweeper = false
+
+  mattr_accessor :recipe_category_sweeper
+  @@recipe_category_sweeper = false
+
+  mattr_accessor :engine_routing
+  @@engine_routing = true
+
   class Engine < Rails::Engine
     # TODO isolate_namespace Recipe
 
@@ -10,10 +19,7 @@ module Recipe
   end
 
   def self.config(&block)
-    @@config ||= Recipe::Engine::Configuration.new
-
-    yield @@config if block
-
-    return @@config
+    yield self if block
+    return self
   end
 end
