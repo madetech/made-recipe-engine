4ff52af5149f416b96a03b51bcbc022922901699
Seb Ashton <seb@madebymade.co.uk>
2013-05-17 11:32:21 +0100

    correct table name

--- db/migrate/20130516104848_add_type_and_video_to_recipe_item.rb
+++ db/migrate/20130516104848_add_type_and_video_to_recipe_item.rb
@@ -1,6 +1,6 @@
 class AddTypeAndVideoToRecipeItem < ActiveRecord::Migration
   def change
-    add_column :recipe_item, :type, :string
-    add_column :recipe_item, :video, :text
+    add_column :recipe_items, :type, :string
+    add_column :recipe_items, :video, :text
   end
 end
