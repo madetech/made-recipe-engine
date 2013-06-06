415a3c0d01511be286219be8a101652ad81d4232
Seb Ashton <seb@madebymade.co.uk>
2013-06-06 18:20:59 +0100

    correct table

--- db/migrate/20130606170621_add_indexes_to_recipe_categorisations.rb
+++ db/migrate/20130606170621_add_indexes_to_recipe_categorisations.rb
@@ -1,6 +1,6 @@
 class AddIndexesToRecipeCategorisations < ActiveRecord::Migration
   def change
-    add_index :recipe_items, :item_id
-    add_index :recipe_items, :category_id
+    add_index :recipe_categorisations, :item_id
+    add_index :recipe_categorisations, :category_id
   end
 end
